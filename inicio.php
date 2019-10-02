0ad442a470d9ee71e7eb5c0b161dae357872d4eb
julieta <julieta.iracheta@hotmail.com>
2019-10-02 11:09:23 -0300

    se loguea correctamente

--- inicio.php
+++ inicio.php
@@ -1,6 +1,42 @@
 <?php
 
+session_start();
 
+echo "<p class='descripcion'>Panel de noticias</p>";
+echo "<div class='menu'><a href='alta.php'>Ingresar nuevo pokemon</a></div>";
+$conexion = mysqli_connect("127.0.0.1","root","46598842","pokemons");
+
+$sql = "SELECT * FROM pokemones";
+$resultado = mysqli_query($conexion,$sql);
+$lista = mysqli_fetch_all($resultado);
+$i=0;
+
+echo "<table>
+              <tr>
+                <td class='nombre'>Nombre</td>
+                <td class='tipo'>Tipo</td>
+                <td class='imagen'>Imagen</td>
+                <td class='editar'>Editar</td>
+                <td class='borrar'>Borrar</td>
+              </tr>";
+
+while($i < count($lista)) {
+    if(empty($lista[$i][3])){
+        $img="";
+    }
+    else{
+        $img="<img src='uploads/".$lista[$i][2]."' width='42'>";
+    }
+
+    echo "<tr>
+                <td align='center'>".$lista[$i][1]."</td>
+                <td style='padding-left: 10px'>".$lista[$i][3]."</td>
+                <td align='center'><a href='modificar.php?id=".$lista[$i][0]."'><img src='img/editar.png'></a></td>
+                <td align='center'><a href='eliminar.php?id=".$lista[$i][0]."'><img src='img/borrar.pg'></a></td>
+              </tr>";
+    $i++;
+}
+echo "</table>";
 ?>
 
-
+<button onclick="location.href='salir.php'">Salir</button>
--- login.php
+++ login.php
@@ -6,24 +6,24 @@ if (isset($_POST['enviar'])) {
     $usuario = $_POST['usuario'];
     $password = $_POST['password'];
 
-    $conexion = mysqli_connect("127.0.0.1","root","46598842","gallo");
+    $conexion = mysqli_connect("127.0.0.1","root","46598842","pokemons");
 
     if(!$conexion){
         echo "ERROR de conexion a la BD";
         die;
     }
 
-    $sql= "SELECT * FROM usuarios WHERE usuario = '".$usuario."'";
+    $sql= "SELECT * FROM usuario WHERE usuario = '".$usuario."'";
     $resultado = mysqli_query($conexion,$sql);
     if(!$resultado){
         $error = "Error al inicar sesion.";
     }else{
         $lista = mysqli_fetch_assoc($resultado);
-        if($lista["clave"] == $password){
+        if($lista["password"] == $password){
 
             session_start();
             $_SESSION['selogueo'] = true;
-            header('location:panel.php');
+            header('location:inicio.html');
             exit();
         }else{
             $error="Error al inicar sesion.2";
@@ -35,5 +35,3 @@ echo "<h2>$error</h2>";
 
 
 ?>
-
-
